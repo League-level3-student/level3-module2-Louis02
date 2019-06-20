dcbb999944eab4929cbb7d1c88219ecb89db6dc4
Louis <louisderrien28@gmail.com>
2019-06-20 14:09:17 -0700

    more and more

--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -6,68 +6,102 @@ import org.junit.Test;
 
 /**
  * Uncomment each test and then write the method to make it pass.
- * **/
+ **/
 
 public class _00_MoreAndMoreTests {
 
-	@Test
-	  public void MultTest() {
+	 @Test
+	 public void MultTest() {
+	
+	 assertEquals("10 x 0 = 0", multiply(10, 0));
+	 assertEquals("10 x 10 = 100", multiply(10, 10));
+	 assertEquals("8 x 11 = 88", multiply(8, 11));
+	 }
 
-	    assertEquals("10 x 0 = 0", multiply(10, 0));
-	    assertEquals("10 x 10 = 100", multiply(10, 10));
-	    assertEquals("8 x 11 = 88", multiply(8, 11));
-	  }
-//
-	@Test
-	  public void PrimeTest() {
+	
+	 @Test
+	 public void PrimeTest() {
+	
+	 assertTrue(isPrime(3));
+	 assertTrue(isPrime(5));
+	 assertTrue(isPrime(541));
+	 assertFalse(isPrime(4));
+	 assertFalse(isPrime(12));
+	 assertFalse(isPrime(527));
+	
+	 }
+	
+	 @Test
+	 public void SquareTest() {
+	
+	 assertTrue(isSquare(4));
+	 assertTrue(isSquare(144));
+	 assertTrue(isSquare(64));
+	 assertTrue(isSquare(10201));
+	 assertTrue(isSquare(1));
+	 assertFalse(isSquare(3));
+	 assertFalse(isSquare(22));
+	 assertFalse(isSquare(143));
+	
+	 }
 
-	    assertTrue(isPrime(3));
-	    assertTrue(isPrime(5));
-	    assertTrue(isPrime(541));
-	    assertFalse(isPrime(4));
-	    assertFalse(isPrime(12));
-	    assertFalse(isPrime(527));
+	private boolean isSquare(int p) {
+		boolean ans = false;
+		for (int i = 0; i < p; i++) {
 
-	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+			if (i * i == p || p == 1) {
+				ans = true;
 
-private boolean isPrime(int i) {
-		// TODO Auto-generated method stub
-		return false;
+			}
+
+		}
+
+		return ans;
 	}
-private Object multiply(int i, int j) {
-	// TODO Auto-generated method stub
-	int ans = -1;
-	ans = i*j;
-	return ans;
-}
 
+	@Test
+	public void CubeTest() {
 
+		assertTrue(isCube(27));
+		assertTrue(isCube(216));
+		assertTrue(isCube(729));
+		assertTrue(isCube(1));
+		assertFalse(isCube(3));
+		assertFalse(isCube(22));
+		assertFalse(isCube(143));
+
+	}
+
+	private boolean isCube(int p) {
+		// TODO Auto-generated method stub
+		boolean ans = false;
+		for (int i = 0; i < p; i++) {
+			if (i * i * i == p || p == 1) {
+				ans = true;
+			}
+
+		}
+		return ans;
+	}
+
+	private boolean isPrime(int i) {
+		// TODO Auto-generated method stub
+		boolean ans = false;
+		for (int k = 0; k < i; k++) {
+		 if (k%i==0) {
+				ans = true;
+			}
+		 else {
+			 ans = false;
+		 }
+		}
+		return ans;
+	}
+
+	private Object multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		String ans = i + " x " + j + " = " + i * j;
+		return ans;
+	}
 
 }
